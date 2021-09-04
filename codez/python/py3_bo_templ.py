a3b1644bcaae7419a1790a27ddfa6cf85e91914e
dualfade <dualfade@vadersecurity.com>
2021-09-04 00:43:40 -0400

    + py3_bo_templ.py

---
+++ codez/python/py3_bo_templ.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# py3_bo_templ.py --
+# dualfade
+
+import sys
+import socket
+from optparse import OptionParser
+
+# globals --
+
+# defs --
+def send():
+    """ send payload -- """
+    port = options.port
+    target = options.target
+
+    try:
+      print('[+] sending payload')
+
+      """ assemble temp sc -- """
+      buffer = b"A" * int(1024)
+
+      """ send payload -- """
+      s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+      s.connect(('%s' % target, port))
+      s.send(buffer)
+      s.close()
+
+      print('[+] done')
+
+    except socket.error as e:
+      print('[!] connection failed')
+      print('[!] socket error: %s' % e)
+      sys.exit(-1)
+
+
+# main --
+if __name__ == "__main__":
+    """ get parser opts -- """
+    parser = OptionParser()
+    parser.add_option('-t', '--target', dest='target', help='target ip address')
+    parser.add_option('-p', '--port', dest='port', help='port')
+    try:
+        (options, args) = parser.parse_args()
+        if (options.target):
+            """ send payload to target -- """
+            print('[+] starting exploit')
+            send()
+            sys.exit(-1)
+        else:
+            print('[!] missing input')
+            print('[!] exiting')
+            sys.exit(-1)
+    except SystemExit:
+        sys.stdout.write("\n")
+        sys.stdout.flush()
