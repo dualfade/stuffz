e8b7847290a6f5a15edda197a96af9472cca2c72
dualfade <dualfade@vadersecurity.com>
2022-04-29 23:59:33 -0400

    python3 bo template update and cleanup

--- codez/python/py3_bo_templ.py
+++ codez/python/py3_bo_templ.py
@@ -1,64 +1,117 @@
 #!/usr/bin/env python3
-# py3_bo_templ.py --
-# dualfade
+# template.py --
+# @dualfade
 
-import sys
+# gen bad chars --
+# pip install badchars
+# badchars -f python | sed 's/^\s*\"./\tb"\\/g' \
+# xclipc -selection clipboard
+
+# skel refs --
+# https://bit.ly/3KIIjzz --
+
 import socket
+import sys
 from optparse import OptionParser
 
-# globals --
+# from struct import pack
 
 # defs --
-def send():
-    """ send payload -- """
+# def eip_chain():
+#     """ eip chain function -- """
+
+#     gadgets = [
+#         0xffffffff,         # ;
+#         0xffffffff,         # ;
+#         0xffffffff,         # ;
+#     ]
+
+#     # return eip --
+#     return b''.join(pack('<L', _) for _ in gadgets)
+
+# def rop_chain():
+#     """ main rop chain function -- """
+
+#     gadgets = [
+#         0xffffffff,         # ;
+#         0xffffffff,         # ;
+#         0xffffffff,         # ;
+#     ]
+
+#     # return eip --
+#     return b''.join(pack('<L', _) for _ in gadgets)
+
+def payload_send():
+    """ send payload function -- """
 
     # args --
-    port = int(options.port)
-    target = str(options.target)
-    buffer = str(options.buffer)
+    length = str(options.length)
 
     try:
-        print('[+] sending payload')
+        print('[info] sending payload')
 
-        # send junk --
-        buffer = b"A" * int(buffer)
+        # initial \x41 test payload --
+        buf = b"\x41" * int(length)
+        sock_connect(buf)
 
-        """ send payload -- """
+    except OSError as err:
+        error(err)
+        sys.exit(-1)
+
+def sock_connect(buffer):
+    """ socket connect function --"""
+
+    try:
+        # args --
+        port = int(options.port)
+        target = str(options.target)
+
+        print("[info]: sending => %s" % buffer)
         s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
         s.connect(('%s' % target, port))
-        print("[info]: sending => %s" % buffer)
         s.send(buffer)
+
+        # get resp --
         resp = s.recv(1024)
         print(resp)
 
-        # close --
+        # finally close socket; ret resp --
         s.close()
 
-        print('[+] done')
+    # except --
+    except OSError as err:
+        error(err)
 
-    except socket.error as e:
-        print('[!] socket error: %s' % e)
-        sys.exit(-1)
+def error(err):
+    """ standard error message -- """
 
+    print('[err] application error %s' % err)
 
 # main --
 if __name__ == "__main__":
-    """ get parser opts -- """
+    """ main function args -- """
+
+    # parser opts --
     parser = OptionParser()
     parser.add_option('-t', '--target', dest='target', help='target ip address')
     parser.add_option('-p', '--port', dest='port', help='port')
-    parser.add_option('-b', '--buffer', dest='buffer', help='buffer length')
+    parser.add_option('-l', '--length', dest='legth', help='payload length')
     try:
         (options, args) = parser.parse_args()
         if (options.target):
-            """ send payload to target -- """
-            print('[+] starting exploit')
-            send()
+
+            # start --
+            print('[info] starting exploit')
+            payload_send()
             sys.exit(-1)
         else:
-            print('[!] missing input')
-            print('[!] exiting')
+            print('[err] missing input')
+            print('[err] exiting')
             sys.exit(-1)
     except SystemExit:
         sys.stdout.write("\n")
         sys.stdout.flush()
+
+
+
+#__eof__
